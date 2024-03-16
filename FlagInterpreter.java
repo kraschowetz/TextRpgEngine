a640940ceb4586b325a2c0e7ca0e3a93b014eac4
Kraschowetz <joao.kraschowetz@gmail.com>
2024-03-16 09:28:09 -0300

    flush flag added

--- FlagInterpreter.java
+++ FlagInterpreter.java
@@ -5,7 +5,9 @@ public class FlagInterpreter {
             case "flag goes here":
                 System.out.println("default flag reached");
                 break;
-        
+            case "flush":
+                System.out.println("\033[H\033[2J");
+                System.out.flush();
             default:
                 break;
         }
